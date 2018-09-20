d2eac0c91b9e6c84ee21da8a8774d19a8b4c4f17
hc660 <31679189+hc660@users.noreply.github.com>
2018-09-20 04:09:34 -0400

    Stopping Apples If They Fall Too Far
    Everything completed up until this point in the chapter.

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	// Prefab for instantiating apples
+	public GameObject applePrefab;
+	
+	// Speed at which the AppleTree moves
+	public float speed = 1f;
+	
+	// Distance where AppleTree turns around
+	public float leftAndRightEdge = 10f;
+	
+	// Chance that the AppleTree will change directions
+	public float chanceToChangeDirections = 0.1f;
+	
+	// Rate at which Apples will be instantiated
+	public float secondsBetweenAppleDrops = 1f;
+	
+	void Start (){
+		// Dropping apples every second
+		Invoke( "DropApple", 2f);
 	}
 	
-	// Update is called once per frame
+	void DropApple () {
+		GameObject apple = Instantiate<GameObject>(applePrefab);
+		apple.transform.position = transform.position;
+		Invoke("DropApple", secondsBetweenAppleDrops);
+	}
+	
 	void Update () {
+		// Basic movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		
+		// Changing Direction
+		if ( pos.x < -leftAndRightEdge ) {
+			speed = Mathf.Abs(speed);
+		} else if ( pos.x > leftAndRightEdge ) {
+			speed = -Mathf.Abs(speed);
+		}
 		
 	}
+	void FixedUpdate () {
+		// Changing Direction Randomly is now time-based because of FixedUpdate()
+		if ( Random.value < chanceToChangeDirections ) {
+			speed *= -1;
+		}
+	}
+	
 }
